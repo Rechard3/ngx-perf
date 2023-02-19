200b7c04564c097e9c59552e82fb827502def241
Omar Abu Saada <omarabusaada93@gmail.com>
2023-02-20 01:00:57 +0300

    fixes build flow and build configuration files
    tsconfig.lib.json was renamed to tsconfig.json
    this allows IDEs to parse it properly
    
    configures development builders to use the source code of the library instead of the built bundle.
    make life much easier when it comes to live updates
    
    fixes typo in production build tsconfig (`tsconfig.lib.prod.json`)
    
    removes redundant library content, all modules are in subpackages
    
    this allows the optimizer to perform better tree shaking

--- projects/ngx-perf/src/public-api.ts
+++ projects/ngx-perf/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-perf
  */
 
-export * from './lib/ngx-perf.service';
-export * from './lib/ngx-perf.component';
-export * from './lib/ngx-perf.module';
+export const ngxPerf = '';
